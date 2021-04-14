e228b9448a1dca6d4f24f2e2d07fa9fb49f50f43
Vadim <yankeevadim@gmail.com>
2021-04-14 12:27:15 +0300

    Demo version
    Demo

---
+++ TestMailSender/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using EmailService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMailSender.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing"
+        };
+
+        private readonly IEmailSender _emailSender;
+
+        public WeatherForecastController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+
+            // create message (how much recipients, subject and body email)
+            Message message = new Message(new string[] { "yankeevadim@gmail.com"}, "It is test email", "Contect from our email");
+            _emailSender.SendEmail(message);
+
+
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
